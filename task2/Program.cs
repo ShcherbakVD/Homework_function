93a94f8ad533ece8b25fa46838b70296344d8402
Elen S <alexia.18@mail.ru>
2024-01-27 15:35:19 +0300

    Решение второй задачи

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int GetEvenNumbers(int[] array)
+{
+    int countEvenNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            countEvenNumbers += 1;
+        }
+    }
+    return countEvenNumbers;
+}
+
+Console.WriteLine("size:");
+int n = Int32.Parse(Console.ReadLine());
+
+int[] array = CreateArray(n);
+int count = GetEvenNumbers(array);
+Console.WriteLine(count);
