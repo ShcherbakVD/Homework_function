7c74e402d467eae196784a9d6dec48f38639d4aa
Elen S <alexia.18@mail.ru>
2024-01-27 22:29:42 +0300

    Решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 2, 7, 5, 9, 8, 6 };
+int inverted;
+for (int i = 0; i < array.Length / 2; i++)
+{
+    inverted = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = inverted;
+}
+
+Console.Write("Новый перевернутый массив: ");
+foreach (int number in array)
+{
+    Console.Write(number + " ");
+}
