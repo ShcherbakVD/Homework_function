24622adc8c2653c150f1348a1b0984ad55b302e5
Elen S <alexia.18@mail.ru>
2024-01-27 14:42:28 +0300

    Решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿void EnterIntegerNumber()
+{
+    while (true)
+    {
+        Console.Write("Введите целое число; для выхода нажмите 'q':");
+        string input = Console.ReadLine();
+        if (input == "q")
+        {
+            break;
+        }
+
+        int number = Convert.ToInt32(input);
+        int sum = 0;
+        while (number >= 1)
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+
+        if (sum % 2 == 0)
+        {
+            break;
+        }
+    }
+}
+
+EnterIntegerNumber();
+Console.Write("STOP");
